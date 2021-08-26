38f864c6616214ada66ccf98b5c73dd8ccad1b83
NehaSijaria <nehasijaria@gmail.com>
2021-08-25 22:37:40 -0400

    Implemented events and move commands to the snake game

--- client.js
+++ client.js
@@ -1,4 +1,5 @@
 // establishes a connection with the game server
+
 const net = require("net");
 const connect = function () {
   const conn = net.createConnection({
@@ -9,18 +10,23 @@ const connect = function () {
   conn.on("connect", () => {
     // code that does something when the connection is first established
     console.log("Successfully connected to game server");
-
   });
   conn.on("connect", () => {
-    // code that does something when the connection is first established
+    // Send Snake name to server
     conn.write("Name: NSS");
-
+    // setInterval(() => {
+    //   conn.write("Move: up");
+    // }, 50);
   });
   // interpret incoming data as text
   conn.setEncoding("utf8");
   conn.on('data', (data) => {
     console.log('Server says: ', data);
 
+  });
+  conn.on('end', () =>{
+    console.log('All the sssnakes are belong to usssss')
+    process.exit()
   });
   return conn;
 };
