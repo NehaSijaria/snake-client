02f6d8e4bd31d6c1e2a7a76e0b4b73e2937cf449
NehaSijaria <nehasijaria@gmail.com>
2021-08-25 20:25:15 -0400

    refactored

--- play.js
+++ play.js
@@ -1,26 +1,5 @@
-const net = require("net");
 
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: '135.23.223.133',
-    port: 50542
-  });
-  conn.on("connect", () => {
-    // code that does something when the connection is first established
-    console.log('HI. THIS IS MY SNAKE GAME WITH LIGHTHOUSE LABS');
-  });
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  conn.on('data', (data) => {
-    console.log('Server says: ', data);
-  });
-
-  return conn;
-};
+const { connect } = require('./client')
 
 console.log("Connecting ...");
 connect();
-
-
